4c893b009ad79e00bd919b5c92f33b98e9d870da
huca01 <huca@kth.se>
2018-05-07 22:08:06 +0200

    Added the knifey-spoon data set

--- KerasVG.py
+++ KerasVG.py
@@ -9,8 +9,9 @@ from tensorflow.python.keras.applications import VGG16
 from tensorflow.python.keras.applications.vgg16 import preprocess_input, decode_predictions
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.python.keras.optimizers import Adam, RMSprop
-from sklearn.cross_validation import train_test_split
 from sklearn.utils.class_weight import compute_class_weight
+from sklearn.metrics import confusion_matrix
+
 
 model = VGG16(include_top=True, weights='imagenet')
 
@@ -101,6 +102,73 @@ def print_layer_trainable():
     for layer in conv_model.layers:
         print("{0}:\t{1}".format(layer.trainable, layer.name))
 
+def example_errors():
+    # The Keras data-generator for the test-set must be reset
+    # before processing. This is because the generator will loop
+    # infinitely and keep an internal index into the dataset.
+    # So it might start in the middle of the test-set if we do
+    # not reset it first. This makes it impossible to match the
+    # predicted classes with the input images.
+    # If we reset the generator, then it always starts at the
+    # beginning so we know exactly which input-images were used.
+    generator_test.reset()
+
+    # Predict the classes for all images in the test-set.
+    y_pred = new_model.predict_generator(generator_test,
+                                         steps=steps_test)
+
+    # Convert the predicted classes from arrays to integers.
+    cls_pred = np.argmax(y_pred, axis=1)
+
+    # Plot examples of mis-classified images.
+    plot_example_errors(cls_pred)
+
+    # Print the confusion matrix.
+    print_confusion_matrix(cls_pred)
+
+
+def print_confusion_matrix(cls_pred):
+    # cls_pred is an array of the predicted class-number for
+    # all images in the test-set.
+
+    # Get the confusion matrix using sklearn.
+    cm = confusion_matrix(y_true=cls_test,  # True class for test-set.
+                          y_pred=cls_pred)  # Predicted class.
+
+    print("Confusion matrix:")
+
+    # Print the confusion matrix as text.
+    print(cm)
+
+    # Print the class-names for easy reference.
+    for i, class_name in enumerate(class_names):
+        print("({0}) {1}".format(i, class_name))
+
+def plot_example_errors(cls_pred):
+    # cls_pred is an array of the predicted class-number for
+    # all images in the test-set.
+
+    # Boolean array whether the predicted class is incorrect.
+    incorrect = (cls_pred != cls_test)
+
+    # Get the file-paths for images that were incorrectly classified.
+    image_paths = np.array(image_paths_test)[incorrect]
+
+    # Load the first 9 images.
+    images = load_images(image_paths=image_paths[0:9])
+
+    # Get the predicted classes for those images.
+    cls_pred = cls_pred[incorrect]
+
+    # Get the true classes for those images.
+    cls_true = cls_test[incorrect]
+
+    # Plot the 9 images we have loaded and their corresponding classes.
+    # We have only loaded 9 images so there is no need to slice those again.
+    plot_images(images=images,
+                cls_true=cls_true[0:9],
+                cls_pred=cls_pred[0:9])
+
 input_shape = model.layers[0].output_shape[1:3]
 
 datagen_train = ImageDataGenerator(
@@ -114,7 +182,6 @@ datagen_train = ImageDataGenerator(
       vertical_flip=True,
       fill_mode='nearest')
 
-
 datagen_test = ImageDataGenerator(rescale=1./255)
 
 batch_size = 20
@@ -124,8 +191,9 @@ if True:
 else:
     save_to_dir='augmented_images/'
 
-train_dir = 'tiny-imagenet-200/train'
-test_dir = 'tiny-imagenet-200/test'
+# train_dir = '../tiny-imagenet-200/train'
+train_dir = './knifey-spoony/train'
+test_dir = './knifey-spoony/test'
 
 generator_train = datagen_train.flow_from_directory(
     directory=train_dir,
@@ -134,16 +202,20 @@ generator_train = datagen_train.flow_from_directory(
     shuffle=True,
     save_to_dir=save_to_dir)
 
-# generator_test = train_test_split(generator_train,test_size=0.1)
 
-# steps_test = generator_test.n / batch_size
+generator_test = datagen_test.flow_from_directory(
+    directory=test_dir,
+    target_size=input_shape,
+    batch_size=batch_size,
+    shuffle=False)
 
+steps_test = generator_test.n / batch_size
 
 image_paths_train = path_join(train_dir, generator_train.filenames)
-# image_paths_test = path_join(test_dir, generator_test.filenames)
+image_paths_test = path_join(test_dir, generator_test.filenames)
 
 cls_train = generator_train.classes
-# cls_test = generator_test.classes
+cls_test = generator_test.classes
 
 class_names = list(generator_train.class_indices.keys())
 num_classes = len(class_names)
@@ -161,7 +233,7 @@ class_weight = compute_class_weight(class_weight='balanced',
                                     classes=np.unique(cls_train),
                                     y=cls_train)
 
-predict(image_path='./tiny-imagenet-200/train/n01644900/images/n01644900_0.JPEG')
+predict(image_path='../tiny-imagenet-200/train/n01644900/images/n01644900_0.JPEG')
 predict(image_path=image_paths_train[0])
 
 
@@ -200,14 +272,14 @@ optimizer = Adam(lr=1e-5)
 loss = 'categorical_crossentropy'
 metrics = ['categorical_accuracy']
 
-print_layer_trainable()
+# print_layer_trainable()
 
 conv_model.trainable = False
 
 for layer in conv_model.layers:
     layer.trainable = False
 
-print_layer_trainable()
+# print_layer_trainable()
 
 new_model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 
@@ -217,4 +289,31 @@ steps_per_epoch = 100
 history = new_model.fit_generator(generator=generator_train,
                                   epochs=epochs,
                                   steps_per_epoch=steps_per_epoch,
-                                  class_weight=class_weight)
+                                  class_weight=class_weight,
+                                  validation_data=generator_test,
+                                  validation_steps=steps_test)
+
+
+plot_training_history(history)
+
+result = new_model.evaluate_generator(generator_test, steps=steps_test)
+print("Test-set classification accuracy: {0:.2%}".format(result[1]))
+example_errors()
+
+conv_model.trainable = True
+
+for layer in conv_model.layers:
+    # Boolean whether this layer is trainable.
+    trainable = ('block5' in layer.name or 'block4' in layer.name)
+
+    # Set the layer's bool.
+    layer.trainable = trainable
+
+optimizer_fine = Adam(lr=1e-7)
+
+history = new_model.fit_generator(generator=generator_train,
+                                  epochs=epochs,
+                                  steps_per_epoch=steps_per_epoch,
+                                  class_weight=class_weight,
+                                  validation_data=generator_test,
+                                  validation_steps=steps_test)
