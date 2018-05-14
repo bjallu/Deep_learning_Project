c3359c738bbd655d821db43ace0ab88665c0f007
huca01 <huca@kth.se>
2018-05-14 17:46:59 +0200

    Q

--- KerasVG.py
+++ KerasVG.py
@@ -238,7 +238,7 @@ datagen_train = ImageDataGenerator(
       fill_mode='nearest')
 
 datagen_test = ImageDataGenerator(rescale=1./255)
-batch_size = 400
+batch_size = 40
 
 if True:
     save_to_dir = None
