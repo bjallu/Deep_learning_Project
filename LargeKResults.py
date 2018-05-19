38a2db4be79141ea3aeb537d16f72534f72ce2ad
huca01 <huca@kth.se>
2018-05-19 14:02:29 +0200

    Q

--- LargeKResults.py
+++ LargeKResults.py
@@ -2,19 +2,23 @@ import numpy as np
 
 number_of_experts = 36
 
-council_resuts = []
+expertName = "0ExpertResults.npy"
+expertResults = np.load(expertName)
 
+council_results = np.zeros_like(expertResults)
+
+power = 1
 for i in range(number_of_experts):
     expertName = str(i) + "ExpertResults.npy"
     expertResults = np.load(expertName)
-    council_resuts += np.power(expertResults, 1)
+    council_results += np.power(expertResults, power)
 
 
 base_predictions = np.load("BaseModelesults.npy")
 true_Classes = np.load("correctClasses.npy")
 
 base_predictions = np.argmax(base_predictions, axis=1)
-expert_predictions = np.argmax(council_resuts, axis=1)
+expert_predictions = np.argmax(council_results, axis=1)
 
 base_correct = 0
 expert_correct = 0
@@ -24,7 +28,7 @@ total = 0
 total_results = []
 
 
-for i in range(base_predictions):
+for i, value in enumerate(base_predictions):
 
     base_p = base_predictions[i]
     expert_p = expert_predictions[i]
@@ -43,7 +47,7 @@ for i in range(base_predictions):
     total_results.append(results)
 
 
-file = open('MichaelPhelps36.txt', 'w+')
+file = open(str(power) + 'MichaelPhelps36.txt', 'w+')
 
 for line in total_results:
     file.write(str(line[0]) + '\t' + str(line[1]) + '\n')
