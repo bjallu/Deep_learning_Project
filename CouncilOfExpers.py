84836c686f99b2986c0c7c799e1dede94b2173fa
huca01 <huca@kth.se>
2018-05-19 10:41:54 +0200

    !

--- CouncilOfExpers.py
+++ CouncilOfExpers.py
@@ -1,25 +1,10 @@
 import matplotlib.pyplot as plt
 import PIL
-import tensorflow as tf
 import numpy as np
 import os
-import time
-from tensorflow.python.keras.models import Model, Sequential
-from tensorflow.python.keras.layers import Dense, Flatten, Dropout
-from tensorflow.python.keras.applications import VGG16
-from tensorflow.python.keras.applications import InceptionV3
-from tensorflow.python.keras.applications import MobileNet
-from tensorflow.python.keras.applications.vgg16 import preprocess_input, decode_predictions
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.python.keras.optimizers import Adam, RMSprop
-from sklearn.utils.class_weight import compute_class_weight
-from sklearn.metrics import confusion_matrix
 from input_pipe import *
 from keras.models import load_model
-import pickle
-from sklearn.cluster import SpectralClustering
-import shutil
-from distutils.dir_util import copy_tree
 
 def path_join(dirname, filenames):
     return [os.path.join(dirname, filename) for filename in filenames]
@@ -77,23 +62,28 @@ steps_test = generator_test.n / batch_size
 
 base_result = base_model.predict_generator(generator_test, steps=steps_test, verbose=1)
 generator_test.reset()
+np.save('BaseModelesults.npy', base_result)
+del base_model
+
+np.save('correctClasses.npy', generator_test.classes)
+
+
 # base_final_result = base_model_final.predict_generator(generator_test, steps=steps_test, verbose=1)
 # generator_test.reset()
 
 council_resuts = np.zeros_like(base_result)
-council_resuts_squared = np.zeros_like(base_result)
 
 for i in range(number_of_experts):
     expertName = str(i) + "MichaelPhelps.h5"
     model = load_model(expertName)
     expert_result = model.predict_generator(generator_test, steps=steps_test, verbose=1)
     generator_test.reset()
-    expert_result_squared = np.power(expert_result, 2)
+
+    np.save(str(i) + 'ExpertResults.npy', expert_result)
     council_resuts += expert_result
-    council_resuts_squared += expert_result_squared
+
     del model
     del expert_result
-    del expert_result_squared
 '''
 for e in expert_model_list:
     expert_result = e.predict_generator(generator_test, steps=steps_test, verbose=1)
@@ -105,7 +95,6 @@ for e in expert_model_list:
 base_predictions = np.argmax(base_result, axis=1)
 # base_final_predictions = np.argmax(base_final_result, axis=1)
 expert_predictions = np.argmax(council_resuts, axis=1)
-expert_predictions_squared = np.argmax(council_resuts_squared, axis=1)
 
 total_results = []
 
@@ -120,7 +109,6 @@ for i, initial in enumerate(base_predictions):
     base_p = base_predictions[i]
     # base_final_p = base_final_predictions[i]
     expert_p = expert_predictions[i]
-    expert_p_squared = expert_predictions_squared[i]
     true_p = generator_test.classes[i]
 
     if(true_p == base_p):
@@ -133,12 +121,9 @@ for i, initial in enumerate(base_predictions):
     if (true_p == expert_p):
         expert_correct += 1
 
-    if (true_p == expert_p_squared):
-        expert_correct_squared += 1
-
     total += 1
 
-    results = [base_correct / total, expert_correct / total, expert_correct_squared/total]
+    results = [base_correct/total, expert_correct/total]
     # results = [base_correct / total, base_final_correct / total, expert_correct / total, expert_correct_squared / total]
     print(results)
     total_results.append(results)
@@ -146,6 +131,6 @@ for i, initial in enumerate(base_predictions):
 file = open('MichaelPhelps36.txt', 'w+')
 
 for line in total_results:
-    file.write(str(line[0]) + '\t' + str(line[1]) + '\t' + str(line[2]) + '\n')
+    file.write(str(line[0]) + '\t' + str(line[1]) + '\n')
 
 file.close()
